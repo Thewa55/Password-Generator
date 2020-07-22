8288cade00202e8d2f3af69e460fab5b5fd72b3b
Kenny Lam <kinwai.lam730@gmail.com>
2020-07-22 15:22:37 -0400

    Added password specification class

--- src/PasswordGenStart.java
+++ src/PasswordGenStart.java
@@ -10,10 +10,13 @@ public class PasswordGenStart {
     public PasswordGenStart(){
         passwordLength();
         System.out.println("The length specified is "+passwordLength);
+        passwordIdentifier();
+//        System.out.println("Upper case "+ upperCase);
+//        System.out.println("Lower case "+ lowerCase);
     }
 
     private void passwordLength(){
-        Scanner in =new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
         do{
             System.out.println("What length would you like the password to be? (6 or greater)");
             if(in.hasNextInt()){
@@ -25,7 +28,31 @@ public class PasswordGenStart {
         } while (passwordLength < 6);
     }
 
-    void passwordIdentifier(){
+    private void passwordIdentifier(){
+        upperCase = passwordSpecifications("uppercase letter");
+        lowerCase = passwordSpecifications("lowercasse letter");
+        numbers = passwordSpecifications("numbers");
+        symbols = passwordSpecifications("symbols");
+        System.out.println("Uppercase: "+ upperCase);
+        System.out.println("Lowercase: "+ lowerCase);
+        System.out.println("Numbers: "+ numbers);
+        System.out.println("Symbols: "+ symbols);
+    }
 
+    private boolean passwordSpecifications(String spec){
+        Scanner in = new Scanner(System.in);
+        String choice;
+        boolean needed = true;
+        System.out.println("Does your password need "+ spec+"? (yes/no)");
+        choice = in.next();
+        if(choice.equals("yes")){
+            needed = true;
+        } else if (choice.equals("no")){
+            needed = false;
+        } else {
+            System.out.println("Invalid input, please enter yes or no");
+            in.next();
+        }
+        return needed;
     }
 }
